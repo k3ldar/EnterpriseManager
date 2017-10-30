f202929c76ab61f4272a394d2e2daebb2c029377
k3ldar <s1cart3r@gmail.com>
2017-10-30 10:38:10 +0000

    Library, language files
    Very little translated at mo :-(

---
+++ Libraries/Languages/LanguageWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Languages
+{
+    public static class LanguageWrapper
+    {
+        /// <summary>
+        /// Gets all installed language files
+        /// </summary>
+        /// <param name="path">Path where search to begin</param>
+        /// <returns>String array of installed languages</returns>
+        public static string[] GetInstalledLanguages(string path)
+        {
+            string[] files = Directory.GetFiles(path, "Languages.resources*", SearchOption.AllDirectories);
+
+            string[] Result = new string[files.Length + 1];
+            Result[0] = "en-GB";
+
+            for (int i = 1; i < Result.Length; i++)
+            {
+                string file = files[i -1].Replace(path, String.Empty);
+
+                file = file.Substring(0, file.IndexOf("\\"));
+                Result[i] = file;
+            }
+
+            return (Result);
+        }
+    }
+}
---
+++ Libraries/Languages/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Languages")]
+[assembly: AssemblyDescription("Language Files")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Shifoo Systems")]
+[assembly: AssemblyProduct("Languages")]
+[assembly: AssemblyCopyright("Copyright © Shifoo Systems 2015 - 2017.  All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bd9e7b12-d4cc-4769-b230-cf10b29527d1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.22.692")]
