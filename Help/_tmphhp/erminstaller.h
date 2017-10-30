96b6e0cc8c74e07775f4784e8284c0be9565d519
k3ldar <s1cart3r@gmail.com>
2017-10-30 10:24:46 +0000

    Help Files

---
+++ Help/_tmphhp/erminstaller.h
@@ -0,0 +1,9 @@
+#define installtype 31
+#define installDestinationlocation 32
+#define installDistributordetails 33
+#define installServer 34
+#define installSelectstartmenu 35
+#define installAdditionaltasks 36
+#define installReadytoinstall 37
+#define installFinish 38
+
---
+++ Help/_tmphhp/gettingstarted.h
@@ -0,0 +1,10 @@
+#define gettingstarted 0
+#define installtype 31
+#define installdestinationlocation 32
+#define installdistributordetails 33
+#define installserver 34
+#define installselectstartmenu 35
+#define installadditionaltasks 36
+#define installreadytoinstall 37
+#define installfinish 38
+
