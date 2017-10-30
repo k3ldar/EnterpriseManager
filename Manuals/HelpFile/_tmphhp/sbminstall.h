011697dd2f667e1ad48f939b5ef257c1f8b4fc9f
k3ldar <s1cart3r@gmail.com>
2017-10-30 11:16:33 +0000

    Manuals and help files

---
+++ Manuals/HelpFile/_tmphhp/sbminstall.h
@@ -0,0 +1,9 @@
+#define installtype 31
+#define destinationlocation 32
+#define distributordetails 33
+#define server 34
+#define selectstartmenu 35
+#define additionaltasks 36
+#define readytoinstall 37
+#define finish 38
+
