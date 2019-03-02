903a5e60ad372f2be72514cef59e79895ce88bbf
k3ldar <s1cart3r@gmail.com>
2019-03-02 07:02:30 +0000

    Update to net461 and package updates

--- Libraries/SalonDiary/Properties/AssemblyInfo.cs
+++ Libraries/SalonDiary/Properties/AssemblyInfo.cs
@@ -55,4 +55,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.29029.1169")]
+[assembly: AssemblyFileVersion("4.0.29029.1172")]
