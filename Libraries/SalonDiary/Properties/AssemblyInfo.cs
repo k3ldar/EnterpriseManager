33b451a329e5988ab8ec61be9d4309cdbe0cf03c
k3ldar <s1cart3r@gmail.com>
2019-03-03 09:07:18 +0000

    Remove assembly signing

--- Libraries/SalonDiary/Properties/AssemblyInfo.cs
+++ Libraries/SalonDiary/Properties/AssemblyInfo.cs
@@ -55,4 +55,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.29029.1186")]
+[assembly: AssemblyFileVersion("4.0.29029.1187")]
